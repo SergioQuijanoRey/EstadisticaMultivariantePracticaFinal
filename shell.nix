33efb2b5d40c0615dc4d7df391f80f9f4fa7ca0a
Sergio Quijano <sergiquijano@gmail.com>
2022-12-22 20:11:11 +0100

    build: añado paquetes de R necesarios

--- shell.nix
+++ shell.nix
@@ -1,24 +1,36 @@
+{ pkgs ? import <nixpkgs> {}}:
+with pkgs;
 # Uso este archivo para tener acceso a pdflatex
 # Define un entorno linux de forma declarativa
-
 let
     # Pinning, because last version of the texlive enviroment is broken
-    pkgs = import (builtins.fetchTarball {
+    pinned_pkgs = import (builtins.fetchTarball {
         name = "NixOS_Stable_05_September_2022";
         url = "https://github.com/nixos/nixpkgs/archive/7d7622909a38a46415dd146ec046fdc0f3309f44.tar.gz";
         sha256 = "016dcmy9gkmrzd5gsmvz4rd6gh5phf2kg7jf8g2y276v5km50nwf";
     }) {};
 
     # Declare the latex enviroment (base enviroment plus additional packages)
-    custom_tex_env = (pkgs.texlive.combine {
+    # We use the pinned pkgs just here
+    custom_tex_env = (pinned_pkgs.texlive.combine {
         # Base latex env
-        inherit (pkgs.texlive) scheme-medium
+        inherit (pinned_pkgs.texlive) scheme-medium
 
         # Se pueden especificar paquetes adicionales, como amsmath
         ;
     });
+
+
+    r_custom_env = rWrapper.override{ packages = with rPackages; [
+        # Para leer el fichero excel
+        readxl
+
+        # Para tener acceso al LSP de R
+        languageserver
+        languageserversetup
+    ]; };
 in
-pkgs.mkShell {
+mkShell {
     buildInputs = with pkgs; [
         # TODO -- remove this dependency
         zsh
@@ -26,8 +38,14 @@ pkgs.mkShell {
         # Build tool for managing latex tasks
         just
 
+        pandoc
+
         # Use our latex enviroment
         custom_tex_env
+
+        # Use our R enviroment
+        r_custom_env
+
     ];
 
     shellHook = ''
