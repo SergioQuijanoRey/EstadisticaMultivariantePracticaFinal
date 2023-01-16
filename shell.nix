8bce42506a700662f44c186a788eb6daf9b72549
Sergio Quijano <sergiquijano@gmail.com>
2023-01-16 12:27:46 +0100

    build, fix: en el shell.nix para desarrollar en R, no tengo que encargarme del entorno Latex

--- shell.nix
+++ shell.nix
@@ -1,26 +1,8 @@
 { pkgs ? import <nixpkgs> {}}:
 with pkgs;
-# Uso este archivo para tener acceso a pdflatex
-# Define un entorno linux de forma declarativa
+# Uso este archivo para declarar todas las dependencias de R necesarias para
+# correr el notebook
 let
-    # Pinning, because last version of the texlive enviroment is broken
-    pinned_pkgs = import (builtins.fetchTarball {
-        name = "NixOS_Stable_05_September_2022";
-        url = "https://github.com/nixos/nixpkgs/archive/7d7622909a38a46415dd146ec046fdc0f3309f44.tar.gz";
-        sha256 = "016dcmy9gkmrzd5gsmvz4rd6gh5phf2kg7jf8g2y276v5km50nwf";
-    }) {};
-
-    # Declare the latex enviroment (base enviroment plus additional packages)
-    # We use the pinned pkgs just here
-    custom_tex_env = (pinned_pkgs.texlive.combine {
-        # Base latex env
-        inherit (pinned_pkgs.texlive) scheme-medium
-
-        # Se pueden especificar paquetes adicionales, como amsmath
-        ;
-    });
-
-
     r_custom_env = rWrapper.override{ packages = with rPackages; [
         # Para leer el fichero excel
         readxl
@@ -56,20 +38,17 @@ let
 in
 mkShell {
     buildInputs = with pkgs; [
-        # TODO -- remove this dependency
+        # Algunas veces necesitamos acceder a la consola
         zsh
 
-        # Build tool for managing latex tasks
+        # Para poder ejecutar las tareas especificadas en `justfile`
         just
 
+        # Rmarkdown tiene esta dependencia
         pandoc
 
-        # Use our latex enviroment
-        custom_tex_env
-
-        # Use our R enviroment
+        # Entorno customizado de R, con todos los paquetes necesarios
         r_custom_env
-
     ];
 
     shellHook = ''
